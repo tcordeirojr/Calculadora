65f37f6378e0a8d68e46e6c06086590dd05ed38a
tcordeirojr <tedcordeirojr@gmail.com>
2024-03-05 03:59:27 -0300

    finalizacao de projeto

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
+function insert(num){
+    document.querySelector(".resultado").innerHTML += num;
+}
+function clean(){
+    document.querySelector(".resultado").innerHTML = "";
 
+}
+function somar(){
+    const tela = document.querySelector(".resultado").innerHTML;
+    if(tela){
+        document.querySelector(".resultado").innerHTML = eval(tela);
+    }else{
+        document.querySelector(".resultado").innerHTML = "Nenhum comando...";
+    }
+}
 
 
 
